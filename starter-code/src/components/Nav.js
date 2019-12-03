e0914c4029767dd567b829412fffc6417758d59c
merrymustard <merryymustard@gmail.com>
2019-12-02 18:00:55 -0600

    compleated

---
+++ starter-code/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav(props) {
+  return (
+    <nav>
+			<img src="./images/ironhack-logo.svg" alt="logo"/>
+			<img src="./images/menu-top.svg" alt="menu"/>
+    </nav>
+  );
+}
+
+export default Nav;
