4f16fe2cb0f76f81fe98d4e06319799d694a6a83
jesus-afb <chucho_quidproquo@hotmail.com>
2019-12-10 01:05:04 -0600

    DP WDFTMEX Jesus Flores

---
+++ starter-code/src/components/HeaderSec.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeaderSec(props) {
+  return (
+    <section className="header">
+			<h2>Say hello to ReactJs</h2>
+			<h4>You will learn a Frontend framework from scratch, to become a Ninja Developer</h4>
+			<button>Awesome</button>
+    </section>
+  );
+}
+
+
+
+export default HeaderSec;
