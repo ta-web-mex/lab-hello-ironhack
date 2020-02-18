c782d5e230961e9bc2eb8618f3aa350f75b1dcc5
arturo <arturo.araujo.alvarez@gmail.com>
2020-02-18 15:00:11 -0600

    your doubts

---
+++ starter-code/src/App.js
@@ -0,0 +1,46 @@
+import React, {
+    Component
+} from 'react'
+import Card from './components/Card'
+import Nav from './components/Nav'
+import Sec from './components/Sec'
+
+
+class App extends Component {
+    render() {
+        return ( <
+            div className = 'generalContainer' >
+            <
+            Nav img1 = "/images/ironhack-logo.svg"
+            img2 = "/images/menu-top.svg" / >
+            <
+            Sec / >
+            <
+            div className = 'allCards' >
+
+            <
+            Card description = "React makes it painless to create interactive UIs"
+            image = "/images/icon1.png" /
+            >
+            <
+            Card description = "Build encapsulated component that manage their state."
+            image = "/images/icon2.png" /
+            >
+            <
+            Card description = "A set of immutable values are passed to cpmponents"
+            image = "/images/icon3.png" /
+            >
+            <
+            Card description = "Statically-typed designed to run on modern browsers."
+            image = "/images/icon4.png" /
+            >
+            <
+            /div>
+
+            <
+            /div>
+        )
+    }
+}
+
+export default App
---
+++ starter-code/src/components/Sec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const Sec = () => {
+    return ( <
+        div >
+        <
+        h1 > Say hello to ReactJS < /h1>   <
+        h3 > You will learn a Frontend framework from scratch, to become a Ninja Developer < /h3> <
+        a > Awesome! < /a>
+
+        <
+        /div>	
+    )
+}
+
+export default Sec
