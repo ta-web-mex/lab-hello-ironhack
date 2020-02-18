9d6725dfb8c65d9f147cb935fca3fef8729cbe66
Edith-LE <edith.lop.estrada@gmail.com>
2020-02-18 08:47:25 -0600

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Card from './components/Card'
+
+class App extends Component {
+    render (){
+        return(
+            <div className="app">
+                <Nav/>
+                <Header/>
+                <Card
+                image="/images/icon1.png"
+                description="React makes it painless to create interactive UIs"
+                />
+                <Card
+                image="/images/icon2.png"
+                description="Build encapsulated component that manage their state"
+                />
+                <Card
+                image="/images/icon3.png"
+                description="A set of immutable values are passed to the components"
+                />
+                <Card
+                image="../public/images/icon4.png"
+                description="Statically-typed designed to run on modern browsers"
+                />
+
+            
+
+            </div>
+        )
+    }
+}
+
+export default App
