8603dbf9ae18f675d0c14b7812ef81c524bea787
israSH <isra-03p@hotmail.com>
2020-02-17 18:33:20 -0600

    React boton y aliniado de imagenes y texto

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -11,17 +11,31 @@ class App extends Component {
         </nav>
         <div>
         <h1> Say hello to ReactJS </h1>
-        <p>You will learn a Forntend framework form scratch, to become a Ninja Developer</p>
+        <h4>You will learn a Forntend framework form scratch, to become a Ninja Developer</h4>
         </div>
-        <section> 
+        <button>
+        <a>Awesome!</a>
+        </button>
+        <section className="cards"> 
+        <div>
         <img src="/images/icon1.png" alt="" />
         <p>React markes it painless to create interactive UIs.</p>
+        </div>
+  
+        <div>
         <img src="/images/icon2.png" alt="" />
         <p>Build encapsulated component that manage their state.</p>
+        </div>
+        <div>
         <img src="/images/icon3.png" alt="" />
+       
         <p>A set f immutable values are passed to the components.</p>
+        </div>
+        <div>
         <img src="/images/icon4.png" alt="" />
+        
         <p>Statically-typed designed to run on modern browsers.</p>
+        </div>
         </section>
         
 
