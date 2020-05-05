cc530b183bc1e9fa82997cb865e9a46235341e8f
Kevin McCallum <kevingmcc@gmail.com>
2020-05-04 22:28:33 -0500

    Done

---
+++ starter-code/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <img src="/images/ironhack-logo.svg" alt="" />
+        <img src="/images/menu-top.svg" alt="" />
+      </div>
+    )
+  }
+}
+
+export default Navbar
